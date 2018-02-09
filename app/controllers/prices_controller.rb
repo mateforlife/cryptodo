cabbb17d10e21a60d2ca73ee7a7fb737d4db8fad
MATEFORLIFE <RODRIGUEZ.RUIZ.ALEX@GMAIL.com>
2018-02-08 21:03:20 -0300

    number_to_currency

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -5,7 +5,9 @@ class PricesController  < ApplicationController
     [
       {name: 'Bitcoin', coin: @Bitcoin = Requester::Coinmarketcap.new('bitcoin', 'CLP')},
       {name: 'Ethereum', coin: @Ethereum = Requester::Coinmarketcap.new('ethereum', 'CLP')},
-      {name: 'Litecoin', coin: @Litecoin = Requester::Coinmarketcap.new('litecoin', 'CLP')}
+      {name: 'Litecoin', coin: @Litecoin = Requester::Coinmarketcap.new('litecoin', 'CLP')},
+      {name: 'Dash', coin: @Dash = Requester::Coinmarketcap.new('dash', 'CLP')},
+      {name: 'Ripple', coin: @Ripple = Requester::Coinmarketcap.new('ripple', 'CLP')}
     ]
   end
 end
